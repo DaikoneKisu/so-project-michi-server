1588d2968a341621d6da0848be8902c62683cbc7
geperezzz <gph393@gmail.com>
2023-01-16 14:05:25 -0400

    Reimplementado el proyecto papa, ahora furula mejor

--- main.cpp
+++ main.cpp
@@ -6,27 +6,41 @@
 #include <thread>
 
 #include <boost/asio/thread_pool.hpp>
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
 #include "javascript interpreter/javascript_interpreter.h"
 #include "http/server.hpp"
 
+using namespace std::string_literals;
+
+std::string generate_random_name() {
+    boost::uuids::random_generator random_uuid_generator;
+    boost::uuids::uuid uuid = random_uuid_generator();
+    return boost::uuids::to_string(uuid);
+}
+
 int main(int argc, char* argv[]) {
+    std::system("mkdir .\\.tmp");
+
     boost::asio::thread_pool thread_pool {std::thread::hardware_concurrency()};
 
-    bool sent_response = {false};
     http::Server http_server {thread_pool.get_executor()};
     http_server
         .on(http::Verb::post, "/cpp",
-            [&sent_response](http::Request& request, http::Response& response, http::ResponseSender send_response) {
-
-                std::ofstream code_to_compile {"code-to-compile.cpp"};
+            [](http::Request& request, http::Response& response, http::ResponseSender send_response) {
+                std::string code_to_compile_filename = ".tmp\\"s + generate_random_name() + ".cpp"s;
+                std::ofstream code_to_compile {code_to_compile_filename};
                 code_to_compile << request.body();
                 code_to_compile.close();
 
-                std::system("g++ -std=c++2a -ocode-to-run code-to-compile.cpp -Iboost_1_81_0 -lwsock32 -lws2_32");
-                //std::system("g++ -std=c++2a code-to-compile.cpp -o code-to-run");
-                std::system("PowerShell -Command \"./code-to-run 127.0.0.1 8080 > code-output.txt \"");
+                std::string executable_filename = ".tmp\\"s + generate_random_name() + ".exe"s;
+                std::string code_output_filename = ".tmp\\"s + generate_random_name() + ".txt"s;
+
+                std::system(("g++ -std=c++2a -o "s + executable_filename + " "s + code_to_compile_filename).data());
+                std::system(("PowerShell -Command \"./"s + executable_filename + " > " + code_output_filename + "\""s).data());
                 
-                std::ifstream code_output {"code-output.txt"};
+                std::ifstream code_output {code_output_filename};
 
                 response.version(11);
                 response.result(http::Status::created);
@@ -36,43 +50,33 @@ int main(int argc, char* argv[]) {
                 response.set(http::Field::content_length, std::to_string(response.body().size()));
                 response.keep_alive(request.keep_alive());
                 send_response();
+
                 code_output.close();
             }
         )
         .on(http::Verb::post, "/js",
-            [&sent_response](http::Request& request, http::Response& response, http::ResponseSender send_response) {
-                
-                std::ofstream code_to_interpret {"code-to-interpret.js"};
+            [](http::Request& request, http::Response& response, http::ResponseSender send_response) {
+                std::string code_to_interpret_filename = ".tmp\\"s + generate_random_name() + ".js"s;
+                std::ofstream code_to_interpret {code_to_interpret_filename};
                 code_to_interpret << request.body();
                 code_to_interpret.close();
 
-                std::system("PowerShell -Command \"node code-to-interpret.js > code-output.txt\"");
+                std::string code_output_filename = ".tmp\\"s + generate_random_name() + ".txt"s;
+
+                std::system(("PowerShell -Command \"node "s + code_to_interpret_filename + " > " + code_output_filename + "\""s).data());
                 
-                std::ifstream code_output {"code-output.txt"};
+                std::ifstream code_output {code_output_filename};
 
                 response.version(11);
                 response.result(http::Status::created);
                 response.set(http::Field::server, "Michi-Server");
-                response.set(http::Field::content_type, "text/plain; charset=utf-8");
+                response.set(http::Field::content_type, "text/plain; charset=utf-16");
                 response.body() = std::string {std::istreambuf_iterator {code_output}, {}};
                 response.set(http::Field::content_length, std::to_string(response.body().size()));
                 response.keep_alive(request.keep_alive());
                 send_response();
+
                 code_output.close();
-            }
-        )
-        .on(http::Verb::get, ".*",
-            [&sent_response](http::Request& request, http::Response& response, http::ResponseSender send_response) {
-                if (sent_response) {
-                    return;
-                }
-                response.version(11);
-                response.result(http::Status::ok);
-                response.set(http::Field::server, "Michi-Server");
-                response.body() = "=^._.^=";
-                response.prepare_payload();
-                response.keep_alive(request.keep_alive());
-                send_response();
             }
         )
         .async_start(argv[1], std::stoi(argv[2]));
